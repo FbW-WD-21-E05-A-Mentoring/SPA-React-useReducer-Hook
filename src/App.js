0142f07e388dc282ba3a53f42123fa93f8a134c0
nrcool <nomanalam7214@gmail.com>
2021-12-10 11:17:57 +0100

    react useReducer hook

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import TodoContainer from './TodoContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TodO App</h1>
+      <TodoContainer/>
+    </div>
+  );
+}
+
+export default App;
